8450667383c8bb0f2898cd84a32cd47a149cca20
Rhys Morgan <rhysm@me.com>
2022-07-12 17:36:23 +0100

    Adds Step1

--- MyApp.swift
+++ MyApp.swift
@@ -1,10 +1,17 @@
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct MyApp: App {
+	let store = Store(
+		initialState: Step1State(),
+		reducer: .step1,
+		environment: Step1Environment(mainQueue: .main)
+	)
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Step1View(store: store)
         }
     }
 }
